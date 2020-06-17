034db92a532a83b5d55f9d65f95a1d4faadff337
Shaligin <redgy1@yandex.ru>
2020-06-18 00:00:34 +0300

    sf

---
+++ src/geekbrains/java/Main.java
@@ -0,0 +1,15 @@
+package geekbrains.java;
+
+public class Main {
+    private static Object String;
+
+    public static void main(String[] args) {
+
+    }
+        public static void change (Object[] arr, int a, int b){
+            Object num = arr[a];
+            arr[a] = arr[b];
+            arr[b] =  arr[a];
+        }
+
+}
