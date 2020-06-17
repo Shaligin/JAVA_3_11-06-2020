5fc776dc831d0e1c5a2c566765eabaef8d43ca30
Shaligin <redgy1@yandex.ru>
2020-06-18 00:11:36 +0300

    sf

--- src/geekbrains/java/Main.java
+++ src/geekbrains/java/Main.java
@@ -1,12 +1,21 @@
 package geekbrains.java;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     private static Object String;
 
     public static void main(String[] args) {
 
     }
-        public static void change (Object[] arr, int a, int b){
+
+    public static <N> ArrayList<N> changeArr(N[] arr){
+        return new ArrayList<N>(Arrays.asList(arr));
+    };
+
+        public static void rechange (Object[] arr, int a, int b){
             Object num = arr[a];
             arr[a] = arr[b];
             arr[b] =  arr[a];
