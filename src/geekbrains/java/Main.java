a26d515ecd1ff8d957073e54116f381eeb6185e8
Shaligin <redgy1@yandex.ru>
2020-06-18 00:26:01 +0300

    sf

--- src/geekbrains/java/Main.java
+++ src/geekbrains/java/Main.java
@@ -8,12 +8,13 @@ public class Main {
     private static Object String;
 
     public static void main(String[] args) {
-
+        int []abc = new int[4];
+        changeArr(); // не пойму как в метод передать массив???
     }
 
     public static <N> ArrayList<N> changeArr(N[] arr){
         return new ArrayList<N>(Arrays.asList(arr));
-    };
+    }
 
         public static void rechange (Object[] arr, int a, int b){
             Object num = arr[a];
