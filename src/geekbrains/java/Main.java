d1e56c10e39927c6544022caf25a3aa3a6f21304
Shaligin <redgy1@yandex.ru>
2020-06-22 00:34:48 +0300

    HW_2

---
+++ src/geekbrains/java/Main.java
@@ -0,0 +1,53 @@
+package geekbrains.java;
+
+import java.sql.*;
+
+public class Main {
+    private PreparedStatement psInsert;
+    private static Connection connect;
+    private static Statement stmn;
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public void start() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connect = DriverManager.getConnection("jdbc:sqllite:MyBoxDB.db");
+            stmn = connect.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getNick(String login, String pass) {
+        try {
+            psInsert = connect.prepareStatement("SELECT * FROM entry WHERE login = ? AND password = ?;");
+            psInsert.setString(1, login);
+            psInsert.setString(2, pass);
+            ResultSet rs = psInsert.executeQuery();
+
+            if (rs.next()) {
+                return rs.getString("nickname");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public void stop() {
+        try {
+            stmn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
